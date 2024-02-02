439fb36486cea813b0f47ad6be70e3a1b6ac3712
Moritz Konrad <moritzkonrad53@gmail.com>
2024-02-02 20:42:16 +0100

    Passende Filterwerte festgelegt. (Versuch alle Filter auf einmal darzustellen hat zu Problemen geführt - noch nicht gelöst)

--- Versuch_3/imageSegmentation.py
+++ Versuch_3/imageSegmentation.py
@@ -1,62 +1,47 @@
 import numpy as np
 import cv2
 
-def mulPixelWithFactor(factor: float,image):
-    width = image.shape[1]
+def filter(image, classFilter, classFarbe):
     height = image.shape[0]
-    for y in range (height):
-        for x in range (width):
-            image[y,x] = image[y,x]*factor
+    width = image.shape[1]
+    b, g, r = cv2.split(image)
+   
+    image2=np.zeros((height,width,3),np.uint8)
+    #Prüft Pixelweise die BGR werte. True wenn alle BGR werte im Filterbereich liegen -> Farbe Weiß wird diesen Pixeln zugewiesen
+    image[((classFilter[0][0] >= b)&(b >= classFilter[1][0])) & ((classFilter[0][1] >= g)&(g >= classFilter[1][1])) & ((classFilter[0][2] >= r)&(r >= classFilter[1][2]))] = classFarbe
     
     return image
 
-def filter(image, classFilter):
-    height = image.shape[0]
-    width = image.shape[1]
-    img_normalized = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
-    b, g, r = cv2.split(img_normalized)
-    #----------------------Weißabgleich----------------------#
-
-    #hellsten Pixel der Kanäle bestimmen
-    bMax = np.max(b)
-    gMax = np.max(g)
-    rMax = np.max(r)
-
-    #Faktor der Kanäle bestimmen
-    kb = 255/bMax
-    kg = 255/gMax
-    kr = 255/rMax
-
-    b = mulPixelWithFactor(kb,b)
-    g = mulPixelWithFactor(kg,g)
-    r = mulPixelWithFactor(kr,r)
-
-    #--------------------------------------------------------#
-
-    image2=np.zeros((height,width,3),np.uint8)
-    #Prüft Pixelweise die BGR werte. True wenn alle BGR werte im Filterbereich liegen -> Farbe Weiß wird diesen Pixeln zugewiesen
-    image2[((classFilter[0][0] >= b)&(b >= classFilter[1][0])) & ((classFilter[0][1] >= g)&(g >= classFilter[1][1])) & ((classFilter[0][2] >= r)&(r >= classFilter[1][2]))] = (255,255,255)
-    
-    return image2
-
 def run(image, result,settings=None):
-    print(image[0,0])
-
+    
     #Filteraufbau
     #[bmax gmax rmax]
     #[bmin gmin rmin]
+    filterYellow = np.array([[87,143,150],[27,100,103]])
+    filterBlue = np.array([[104,60,85],[63,20,0]])
+    filterOrange = np.array([[95,91,145],[0,20,110]])
+    filterRot = np.array([[69,57,95],[0,0,60]])
+    filterGrün = np.array([[76,87,10],[40,50,0]])
+    filterHintergrund = np.array([[179,164,140],[129,120,100]])
+
+    #Markierungsfarben die den gefilterten Bereichen (Klassen) zugewiesen werden
     #bgr Wertebereiche für die Farben: http://www.markusbader.de/tricky/rgb_blau.html
-    filterClassYellow = np.array([[settings[0],255,255],[settings[1],105,139]])
-    filterClassBackround = np.array([[144,130,112],[134,120,112]])#pixel[0,0]=[139 125 107] -> +-5 bei Filter
-    #filterClassBlue = np.array([[255,255,240],[112,0,0]])
-    #filterClassBlueVio = np.array([[255,255,255],[96,0,0]])
+    klasseGelb = (0,255,255)
+    klasseBlau = (238,0,0)
+    klasseOrange = (0,127,255)
+    klasseRot = (0,0,238)
+    klasseGrün = (0,255,0)
+    klasseHintergrund = (255,255,255)
 
+    
+    result.append({"name":"Filter Gelb","data":filter(image,filterYellow, klasseGelb)})
+    result.append({"name":"Filter Blau","data":filter(image,filterBlue, klasseBlau)})
+    result.append({"name":"Filter Orange","data":filter(image,filterOrange, klasseOrange)})
+    result.append({"name":"Filter Rot","data":filter(image,filterRot, klasseRot)})
+    result.append({"name":"Filter Grün","data":filter(image,filterGrün, klasseGrün)})
+    result.append({"name":"Filter Hintergrund","data":filter(image,filterHintergrund, klasseHintergrund)})
 
-    result.append({"name":"Original","data":image})
-    result.append({"name":"Filter Gelb","data":filter(image,filterClassYellow)})
-    result.append({"name":"Filter Hintergrund","data":filter(image,filterClassBackround)})
-    #result.append({"name":"Filter Gelb Costum","data":filter(image,filterClassYellowCostum)})
-    #result.append({"name":"Filter Blau Violett","data":filter(image,filterClassBlueVio)})
+    #result.append({"name":"Filter Alle 6 Klassen","data":filter(filter(filter(filter(filter(filter(image,filterYellow, klasseGelb),filterBlue, klasseBlau),filterOrange, klasseOrange),filterRot, klasseRot),filterGrün, klasseGrün),filterHintergrund, klasseHintergrund)})
 
 if __name__ == '__main__':
     image=cv2.imread("Images\Farbpunkte.jpg")
